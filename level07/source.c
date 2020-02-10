cd9dfae1236ffd68787a234a6af80728411be746
Kikoman90 <fsidler@student.42.fr>
2020-02-11 00:55:25 +0100

    Aborting commit due to empty commit message.

--- level07/source.c
+++ level07/source.c
@@ -1,13 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 
-int	main(int argc, char **argv, char **envp)
+void		clear_stdin(void)
 {
-	char	**av = argv;
-	char	**ep = envp;
-	int	ret = 0;
-	char	input[20] = {0};
-	char	buff[100] = {0};
+	unsigned char c = 0;
+
+	while (c != (unsigned char)EOF)
+	{
+		if ((c = (unsigned char)getchar()) == '\n')
+			return ;
+	}
+}
+
+unsigned int	get_unum(void)
+{
+	unsigned int unum = 0;
+	
+	fflush(stdout);
+	scanf("%u", &unum);
+	clear_stdin();
+	return (unum);
+}
+
+unsigned int	store_number(unsigned int *buffer)
+{
+	unsigned int num, idx = 0;
+
+	printf(" Number: ");
+	num = get_unum();
+	printf(" Index: ");
+	idx = get_unum();
+	if (idx % 3 == 0 || (num >> 24) == 183)
+	{
+		puts(" *** ERROR! ***");
+		puts("   This index is reserved for wil!");
+		puts(" *** ERROR! ***");
+		return (1);
+	}
+	buffer[idx] = num;
+	return (0);
+}
+
+int		read_number(unsigned int *buffer)
+{
+	unsigned int idx = 0;
+	
+	printf(" Index: ");
+	idx = get_unum();
+	printf(" Number at data[%u] is %u\n", idx, buffer[idx]);
+	return (0);
+}
+
+int		main(int argc, char **argv, char **envp)
+{
+	char		**av = argv;
+	char		**ep = envp;
+	unsigned int	ret = 0;
+	char		input[20] = {0};
+	unsigned int	tab[100] = {0};
 
 	while (*av)
 	{
@@ -19,8 +69,33 @@ int	main(int argc, char **argv, char **envp)
 		memset(*ep, 0, strlen(*ep));
 		ep++;
 	}
-	puts("----------------------------------------------------\n  Welcome to wil's crappy number storage service!   \n----------------------------------------------------\n Commands:                                          \n    store - store a number into the data storage    \n    read  - read a number from the data storage     \n    quit  - exit the program                        \n----------------------------------------------------\n   wil has reserved some storage :>                 \n----------------------------------------------------\n");
-	printf("Input command: ");
-	fgets(input, 20, stdin);
-	input[strlen(input) - 1] = 0;	
+	puts(	"----------------------------------------------------\n"\
+		"  Welcome to wil's crappy number storage service!   \n"\
+		"----------------------------------------------------\n"\
+	       	" Commands:                                          \n"\
+	    	"    store - store a number into the data storage    \n"\
+	    	"    read  - read a number from the data storage     \n"\
+	    	"    quit  - exit the program                        \n"\
+		"----------------------------------------------------\n"\
+	     	"   wil has reserved some storage :>                 \n"\
+		"----------------------------------------------------\n");
+	while (1)
+	{
+		printf("Input command: ");
+		ret = 1;
+		fgets(input, 20, stdin);
+		input[strlen(input) - 1] = 0;
+		if (strncmp(input, "store", 5) == 0)
+			ret = store_number(tab);
+		else if (strncmp(input, "read", 4) == 0)
+			ret = read_number(tab);
+		else if (strncmp(input, "quit", 4) == 0)
+			break;
+		if (ret == 0)
+			printf(" Completed %s command successfully\n", input);
+		else
+			printf(" Failed to do %s command\n", input);
+		memset(input, 0, 20);
+	}
+	return (0);
 }
