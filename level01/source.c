af12a9a473fbeaef36db2eafb6ac1b100e37b1ef
Francois Sidler <fsidler@student.42.fr>
2022-02-10 20:56:15 +0100

    negligible change

--- level01/source.c
+++ level01/source.c
@@ -11,7 +11,7 @@ int	verify_user_name(void)
 
 int	verify_user_pass(char *pass)
 {
-	return (strncmp(pass, "admin", 5));	
+	return (strncmp(pass, "admin", 5)); // returns -1, 0 or 1
 }
 
 int	main(void)
@@ -28,16 +28,16 @@ int	main(void)
 		return (1);
 	}
 	puts("Enter Password: ");
-	fgets(pass, 100, stdin);
+	fgets(pass, 100, stdin); // vulnerable to overflow
 	if ((ret = verify_user_pass(pass)) == 0)
 	{
 		puts("nope, incorrect password...\n");
-		return (1);
+		return (1);	
 	}
-	else if (ret != 0)
+	else if (ret == 0)
 	{
-		puts("nope, incorrect password...\n");
-		return (1);
+		return (0);
 	}
-	return (0);
+	puts("nope, incorrect password...\n");
+	return (1);
 }
